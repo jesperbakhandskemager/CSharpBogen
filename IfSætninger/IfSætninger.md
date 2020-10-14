e8ae950ad6c67c525ee0c3b5881a1b9751a65459
Mads Bock Christensen <bockchristensen@gmail.com>
2020-10-14 09:19:59 +0200

    Merge

--- IfSætninger/IfSætninger.md
+++ IfSætninger/IfSætninger.md
@@ -1,1 +1,88 @@
-# If Sætninger
+# Kapitel 3 If Sætninger
+I programmering har man tit brug for at analysere det data man har gemt, og tage et valg om hvad man skal gøre. Til dette bruger vi _if sætninger_, som kan tage et valg på baggrund af en boolean værdi.
+
+## If
+Mønsteret for en If Sætning ser således ud
+```csharp
+if(<bool>) {
+    //run if true
+}
+```
+
+Det betyder at med følgende kode kan vi tjekke om værdien af en bool variabel er noget bestemt.
+```csharp
+bool myBooleanVariable;
+if(myBooleanVariable) {
+    Console.WriteLine("myBooleanVariable er sat til Sand");
+}
+```
+
+Ofte tester man dog ikke direkte på boolean variabler. Eksempelvis kan det være man gerne vil sammenligne to variabler for at se om de er ens. Her kan vi bruge vores viden om operatorer til at omdanne de to værdier til én boolean værdi, som i følgende eksempel.
+```csharp
+int number1 = 15;
+int number2 = 15;
+
+//Tjek om de er ens
+if(number1 == number2) {
+    Console.WriteLine("De to værdier er ens");
+}
+
+//Tjek om number1 er mindre end number2
+if(number1 < number2) {
+    Console.WriteLine("Den ene er mindre end den anden")
+}
+```
+
+Fordi vi ved at sammenlignings operatorer tager 2 værdier og laver om til én boolean værdi, kan vi konkludere at de kan bruges i if sætninger.
+
+## Else
+Nogen gange kan det være nødvendigt at definere noget kode som skal køre hvis en if sætning _ikke_ kører. Til dette kan man udvide en if sætning med en else sætning. Det følger følgende mønster
+```csharp
+if(<bool>) {
+    //run if true
+} else {
+    //run if false
+}
+```
+
+Det vil sige at vi kunne udvide ```myBooleanVariable``` eksemplet ovenfra
+```csharp
+bool myBooleanVariable;
+if(myBooleanVariable) {
+    Console.WriteLine("myBooleanVariable er sat til Sand");
+} else {
+    Console.WriteLine("myBooleanVariable er sat til Falsk");
+}
+//Kun en af de ovenstående linjer vil blive skrevet i konsollen.
+```
+
+Koden i else sætningen kører kun hvis den foregående if sætning ikke har evalueret til sand. Med andre ord kan if-koden og else-koden ikke begge to køre.
+
+## Else If
+Til mere avanceret logik kan det være nødvendigt at bruge _Else If_. 
+
+```csharp
+if(<bool1>) {
+    //run if bool1 is true
+} else if(<bool2>) {
+    //run if bool2 is true
+} else {
+    //run if neither is true
+}
+```
+
+Else If igangsætter en ny if sætning efter den foregående er fejlet. Den nye if sætning vil kun lave evaluering hvis den første if sætning er fejlet. Man kan lave flere end en else if i sin sturktur, som i eksemplet herunder. 
+```csharp
+int n;
+if(n < 0) {
+    Console.WriteLine("N is very small");
+} else if(n < 10) {
+    Console.WriteLine("N is small");
+} else if(n < 20) {
+    Console.WriteLine("N is big");
+} else if(n < 30) {
+    Console.WriteLine("N is very big");
+} else {
+    Console.WriteLine("N is really, really big");
+}
+```
--- IfSætninger/Program.cs
+++ IfSætninger/Program.cs
@@ -7,9 +7,9 @@ namespace IF_Sætninger
         static void Main(string[] args)
         {
             //If sætninger kan bruges til at lave betingelser for udførsel af kode.
-            
+
             //TODO Opgave 1: gør således at følgende if sætning faktisk kører og der bliver skrevet "opgave 1" i konsollen.
-            if(false)
+            if (false)
             {
                 Console.WriteLine("opgave 1");
             }
@@ -20,46 +20,91 @@ namespace IF_Sætninger
             //TODO Opgave 2: Følgende if sætning bruger en boolean variabel til at afgøre om den skal køre eller ej. Gør således at denne kode kører, og "opgave 2" bliver skrevet i konsollen.
             //NOTE der er to veje til at løse denne opgave
             bool opg2ShouldRun = false;
-            if(opg2ShouldRun)
+            if (opg2ShouldRun)
             {
                 Console.WriteLine("opgave 2");
             }
 
 
             //Fordi if sætninger tager imod en boolean værdi kan vi bruge vores viden om operatorer fra sidste kapitel til at gøre dem smarte.
-            const int a = 3;
-            const int b = 7;
-            const int c = 5;
+            //TODO Opgave 3,4 og 5: Ændre på værdierne for a, b og c således at der ikke bliver printet "Error" nogen steder, og at der bliver printet hhv. opgave 3, opgave 4 og opgave 5.
+            const int a = 1;
+            const int b = 1;
+            const int c = 1;
 
-            if(a > c)
+            if (a > c)
             {
                 Console.WriteLine("Error");
             }
 
-            if(a < c)
+            if (a < c)
             {
                 Console.WriteLine("opgave 3");
             }
 
-            if(a == b && a < c)
+            if (a == b && a < c)
             {
                 Console.WriteLine("error");
             }
 
-            if(c < b)
+            if (c < b)
             {
                 Console.WriteLine("opgave 4");
             }
 
-            if(a != a)
+            if (a != a)
             {
                 Console.WriteLine("error");
             }
 
-            if(a < b || a == c)
+            if (a < b || a == c)
             {
                 Console.WriteLine("opgave 5");
             }
+
+
+            //Man kan udvide sin if sætning med en "else sætning", for at beskrive hvad der skal ske hvis if sætningen får en false boolean værdi.
+            //TODO Opgave 6: Ændre på d således at "opgave 6" bliver printet i konsollen.
+            const int d = 3;
+            if (d < 5)
+            {
+                Console.WriteLine("Error");
+            } else
+            {
+                Console.WriteLine("opgave 6");
+            }
+
+
+            //Og man kan også udvide med en if-else sætning
+            //TODO Opgave 7: Ændre på e således at "opgave 7" bliver printet i konsollen.
+            const int e = 0;
+            if (e < 10)
+            {
+                Console.WriteLine("Error");
+            } else if (e < 0)
+            {
+                Console.WriteLine("opgave 7");
+            }
+
+            /* Hvis du har lavet opgaverne rigtigt indtil videre, så burde der nu stå
+             * opgave 1
+             * opgave 2
+             * opgave 3
+             * opgave 4
+             * opgave 5
+             * opgave 6
+             * opgave 7
+             */
+
+
+
+            /* TODO Opgave 8: Du skal nu lave en if struktur. Den skal have afhænge af ovenstående to variabler, foo og bar
+             * Hvis foo er mindre end 15 skal du tjekke om bar er større end foo. Hvis den er skriver du Works. Ellers skriver du Error.
+             * Ellers skal du tjekke om bar er mindre end 15, og skrive error i konsollen hvis den er. Ellers skriver du Works.
+             * Hvis foo er større eller ligmed 15 
+             * Test gerne med forskellige værdier af foo og bar
+             */
+
 
         }
     }
